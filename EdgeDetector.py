e4a6471a6898abfee88d61b6e581a0de9e7e7b9b
haaajh <dldudvy777@gmail.com>
2022-02-27 21:18:21 +0900

    initial commit

---
+++ EdgeDetector.py
@@ -0,0 +1,6 @@
+import cv2
+
+img1 = cv2.imread('dataset/img1.jpg', cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread('dataset/img2.jpg', cv2.IMREAD_GRAYSCALE)
+img3 = cv2.imread('dataset/img3.jpg', cv2.IMREAD_GRAYSCALE)
+img4 = cv2.imread('dataset/img4.jpg', cv2.IMREAD_GRAYSCALE)
