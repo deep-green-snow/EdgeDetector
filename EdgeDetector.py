5c144d0f49845d71b83609c721f60300ffee66e6
haaajh <dldudvy777@gmail.com>
2022-02-28 08:51:51 +0900

    plt show grayscale

--- EdgeDetector.py
+++ EdgeDetector.py
@@ -4,23 +4,23 @@ import matplotlib.pyplot as plt
 
 
 def sobel(src):
-    sobel_x = np.array([[-1, -2, -1], 
+    sobel_x = np.array([[ 1,  2,  1], 
                         [ 0,  0,  0], 
-                        [ 1,  2,  1]]) 
-    sobel_y = np.array([[1, 0, -1], 
-                        [2, 0, -2], 
-                        [1, 0, -1]])
+                        [-1, -2, -1]]) 
+    sobel_y = np.array([[-1, 0, 1], 
+                        [-2, 0, 2], 
+                        [-1, 0, 1]])
     xdir = cv2.convertScaleAbs(cv2.filter2D(src, cv2.CV_32F, sobel_x))
     ydir = cv2.convertScaleAbs(cv2.filter2D(src, cv2.CV_32F, sobel_y))
     return cv2.addWeighted(xdir, 1, ydir, 1, 0);
 
 def prewitt(src):
-    prewitt_x = np.array([[-1, -1, -1], 
+    prewitt_x = np.array([[ 1,  1,  1], 
                           [ 0,  0,  0], 
-                          [ 1,  1,  1]]) 
-    prewitt_y = np.array([[1, 0, -1], 
-                          [1, 0, -1], 
-                          [1, 0, -1]]) 
+                          [-1, -1, -1]]) 
+    prewitt_y = np.array([[-1, 0, 1], 
+                          [-1, 0, 1], 
+                          [-1, 0, 1]]) 
     xdir = cv2.convertScaleAbs(cv2.filter2D(src, -1, prewitt_x))
     ydir = cv2.convertScaleAbs(cv2.filter2D(src, -1, prewitt_y))
     return cv2.addWeighted(xdir, 1, ydir, 1, 0);
@@ -40,9 +40,9 @@ def scharr(src):
     scharr_x = np.array([[ 3, 10, 3], 
                          [ 0,  0, 0], 
                          [-3,-10,-3]]) 
-    scharr_y = np.array([[ 3, 0,  -3], 
-                         [10, 0, -10], 
-                         [ 3, 0,  -3]])
+    scharr_y = np.array([[ -3, 0,  3], 
+                         [-10, 0, 10], 
+                         [ -3, 0,  3]])
     xdir = cv2.convertScaleAbs(cv2.filter2D(src, -1, scharr_x))
     ydir = cv2.convertScaleAbs(cv2.filter2D(src, -1, scharr_y))
     return cv2.addWeighted(xdir, 1, ydir, 1, 0);
@@ -66,12 +66,23 @@ def blob(src):
 
     return cv2.drawKeypoints(src, kp, np.zeros((1,1)), (255,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
+def compass(src):
+    return
+def lineEdge(src):
+    return
+
+# https://blog.daum.net/trts1004/12109067
+
 def laplacian(src):
     return cv2.Laplacian(src, -1)
+
 def LoG(src):
-    mask = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+    mask = np.array([[-1, -1, -1], 
+                     [-1,  8, -1], 
+                     [-1, -1, -1]])
     gaussian = cv2.GaussianBlur(src, (5, 5), 0) 
     return cv2.filter2D(gaussian, -1, mask)
+
 def DoG(src):
     height, width = src.shape
     gaussian1 = cv2.GaussianBlur(src, (5, 5), 1.6) 
@@ -82,7 +93,6 @@ def DoG(src):
             DoG[i][j] = float(gaussian1[i][j]) - float(gaussian2[i][j])
 
     return DoG
-
 
 if __name__ == "__main__": 
     img1_color = cv2.imread('dataset/img1.jpg')
@@ -95,8 +105,8 @@ if __name__ == "__main__":
     img3_gray = cv2.cvtColor(img3_color, cv2.COLOR_BGR2GRAY)
     img4_gray = cv2.cvtColor(img4_color, cv2.COLOR_BGR2GRAY)
 
-    img_color = img4_color
-    img_gray = img4_gray
+    img_color = img1_color
+    img_gray = img1_gray
     
     fig, ax = plt.subplots(3,3, figsize=(10,10), sharey=True)
     fig.canvas.manager.set_window_title('compare edge detector')
@@ -122,14 +132,14 @@ if __name__ == "__main__":
     ax[2][2].set_title('DoG')
     
     ax[0][0].imshow(img_color, aspect="auto")
-    ax[0][1].imshow(sobel(img_gray), aspect="auto")
-    ax[0][2].imshow(prewitt(img_gray), aspect="auto")
-    ax[1][0].imshow(roberts(img_gray), aspect="auto")
-    ax[1][1].imshow(scharr(img_gray), aspect="auto")
-    ax[1][2].imshow(canny(img_gray), aspect="auto")
-    ax[2][0].imshow(blob(img_gray), aspect="auto")
-    ax[2][1].imshow(LoG(img_gray), aspect="auto")
-    ax[2][2].imshow(DoG(img_gray), aspect="auto")
+    ax[0][1].imshow(sobel(img_gray), aspect="auto", cmap='gray')
+    ax[0][2].imshow(prewitt(img_gray), aspect="auto", cmap='gray')
+    ax[1][0].imshow(roberts(img_gray), aspect="auto", cmap='gray')
+    ax[1][1].imshow(scharr(img_gray), aspect="auto", cmap='gray')
+    ax[1][2].imshow(canny(img_gray), aspect="auto", cmap='gray')
+    ax[2][0].imshow(blob(img_gray), aspect="auto", cmap='gray')
+    ax[2][1].imshow(LoG(img_gray), aspect="auto", cmap='gray')
+    ax[2][2].imshow(DoG(img_gray), aspect="auto", cmap='gray')
     
     # dstName = "data/myFig.png"
     # plt.savefig(dstName, bbox_inches="tight")
